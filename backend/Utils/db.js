2e661c0c34cba8431565bb179a8afd335516e625
Granthj <dasgranth24@gmail.com>
2026-05-04 15:06:54 +0530

    updated db.js file

--- backend/Utils/db.js
+++ backend/Utils/db.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('expensesdb', 'root', 'root', {
-    host: 'localhost',
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 });
 
@@ -8,7 +13,8 @@ const sequelize = new Sequelize('expensesdb', 'root', 'root', {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
-    } catch (error) {
+    } 
+    catch (error) {
         console.error('Unable to connect to the database:', error);
     }
 })();
